aceb4996780822a676eb3bb4986c1e561814513f
bhecra <ingchristianburbano@gmail.com>
2023-10-28 16:45:30 -0500

    add delete item in crud and add eslint config

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
+import { ICar } from './interfaces/car.interface';
+import { UpdateCarDto } from './dto';
 
 @Controller('cars')
 export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
   @Get()
-  getAllCars() {
-    return ['Mazda', 'Toyota', 'Renault'];
+  getAllCars(): ICar[] {
+    return this.carsService.getAllCars();
+  }
+
+  @Get(':id')
+  getCarById(@Param('id', ParseUUIDPipe) id: string): ICar {
+    return this.carsService.getOneById(id);
+  }
+  @Post()
+  createCar(@Body() body: CreateCarDto) {
+    return this.carsService.createCar(body);
+  }
+  @Patch(':id')
+  updateCar(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateCarDto: UpdateCarDto,
+  ) {
+    return this.carsService.updateCar(id, updateCarDto);
+  }
+  @Delete(':id')
+  deleteCar(@Param('id', ParseUUIDPipe) id: string) {
+    return this.carsService.deleteCar(id);
   }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { UpdateCarDto, CreateCarDto } from './dto';
 @Injectable()
-export class CarsService {}
+export class CarsService {
+  private cars = [
+    {
+      id: uuid(),
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: uuid(),
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: uuid(),
+      brand: 'Jeep',
+      model: 'Cherokee',
+    },
+    {
+      id: uuid(),
+      brand: 'Mazda',
+      model: 'CX-30',
+    },
+  ];
+
+  getAllCars() {
+    return this.cars;
+  }
+  getOneById(id: string) {
+    const car = this.cars.find((car) => car.id == id);
+
+    if (!car) {
+      throw new NotFoundException(`Car with id ${id} not found`);
+    }
+
+    return car;
+  }
+
+  createCar(car: CreateCarDto) {
+    const newCar = { id: uuid(), ...car };
+    this.cars.push(newCar);
+    return newCar;
+  }
+  updateCar(id: string, updateCarDto: UpdateCarDto) {
+    let carDb = this.getOneById(id);
+    this.cars = this.cars.map((car) => {
+      if (car.id === id) {
+        carDb = {
+          ...carDb,
+          ...updateCarDto,
+          id,
+        };
+
+        return carDb;
+      }
+      return car;
+    });
+    return carDb;
+  }
+  deleteCar(id: string) {
+    return this.cars.filter((car) => car.id != id);
+  }
+}
