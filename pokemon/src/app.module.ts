13ae27b96f5c22549d5b4341c1d5dd4364b582dd
cburbano <cburbano@ubits.co>
2023-10-28 20:34:47 -0500

    add static site

--- pokemon/src/app.module.ts
+++ pokemon/src/app.module.ts
@@ -1,9 +1,15 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
