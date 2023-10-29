983e30fb41da3acca02fb7a1e70ebc172b2a64ef
cburbano <cburbano@ubits.co>
2023-10-28 22:41:51 -0500

    settings and pipe

--- pokemon/src/app.module.ts
+++ pokemon/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
     }),
     MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon'),
     PokemonModule,
+    CommonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
