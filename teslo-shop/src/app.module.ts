70eabe50af689cb1621327ca8caed0e4b03529fc
cburbano <cburbano@ubits.co>
2023-10-29 14:16:10 -0500

    add products resource

--- teslo-shop/src/app.module.ts
+++ teslo-shop/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    ProductsModule,
   ],
   controllers: [],
   providers: [],
