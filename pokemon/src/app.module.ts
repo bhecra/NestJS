8e4b3a8d66639a760cbc07fa6704673418b1ddfb
cburbano <cburbano@ubits.co>
2023-10-28 21:20:01 -0500

    add pokemon Module

--- pokemon/src/app.module.ts
+++ pokemon/src/app.module.ts
@@ -3,12 +3,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon'),
+    PokemonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
